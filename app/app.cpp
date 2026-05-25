f1540e59fb8bd95b98e9c8c8e8b1085ac3c5934e
Maidui3 <158636271+Maidui3@users.noreply.github.com>
2026-05-25 21:50:55 +0900

    PID追加

--- app/app.cpp
+++ app/app.cpp
@@ -5,6 +5,7 @@
 #include "tim.h"
 //
 
+#include "../maidui3_hal/Control/PID/Proportional-Integral-Derivative.hpp"
 #include "../maidui3_hal/Drivers/FDCAN/mXCAN.hpp"
 #include "driver_fdcan.hpp"
 #include "gn10_can/core/fdcan_bus.hpp"
@@ -20,7 +21,6 @@ gn10_can::devices::ESCHubConfig hub_config;
 #define C620_bace_id 0x200
 
 maidui3_xcan::xcan esc_bus_can(&hfdcan2, maidui3_xcan::fifo::FIFO1, maidui3_xcan::id_filter_type::mask_four_id, 0, 1);
-
 maidui3_xcan::hxcan_frame c620_s;
 maidui3_xcan::hxcan_frame c620_r[4];
 
@@ -47,27 +47,21 @@ struct c620_speed_box {
     float rad_p_s       = {0};
 };
 
-c620_speed_box c620_line_table[4][800];
-c620_speed_box c620_diagonal_table[4][800];
-c620_speed_box c620_rotate_table[4][800];
-
-int16_t c620_rad(int16_t rad)
-{
-    return 0;
-}
-
-int16_t c620_current(int16_t current)
+int16_t c620_current_current(int16_t current)
 {
     return (int16_t)((current & 0xFF) << 8) | ((current & 0xFF00) >> 8);
 }
 
-void three_omni_cab();
-void four_omni_cab();
+int16_t c620_rad_to_current(int16_t rad)
+{
+    return c620_current_current(0);
+}
 
-float PID(float diff);
-float P(float diff);
-float I(float diff);
-float D(float diff);
+// CAN
+
+maidui3_hal::Control::PID::Proportional_Integral_Derivative PID[4];
+
+// PID
 
 void Error();
 
@@ -75,6 +69,18 @@ void setup()
 {
     HAL_Delay(10);
     HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, GPIO_PIN_SET);
+
+    c620_s.id_     = C620_bace_id;
+    c620_s.len_    = 0x08;
+    c620_s.data_p_ = c620_send.data;
+
+    for (uint8_t i = 0; i < 4; i++) {
+        c620_r[i].id_     = 0x00;
+        c620_r[i].len_    = 0x00;
+        c620_r[i].data_p_ = c620_receive[i].data;
+    }
+
+    /*領域の定義*/
 
     esc_bus_can.set_Id(C620_bace_id + 1);
     esc_bus_can.set_Id(C620_bace_id + 2);
@@ -87,16 +93,6 @@ void setup()
         HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, GPIO_PIN_SET);
     }
 
-    c620_s.id_     = C620_bace_id;
-    c620_s.len_    = 0x08;
-    c620_s.data_p_ = c620_send.data;
-
-    for (uint8_t i = 0; i < 4; i++) {
-        c620_r[i].id_     = 0x00;
-        c620_r[i].len_    = 0x00;
-        c620_r[i].data_p_ = c620_receive[i].data;
-    }
-
     c620_send.value[0] = 0x00;
     c620_send.value[1] = 0x00;
     c620_send.value[2] = 0x00;
@@ -104,21 +100,36 @@ void setup()
 
     if (esc_bus_can.SendMessage(&c620_s)) {
         HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, GPIO_PIN_SET);
+        /*送信できてない*/
     } /*全ESC停止*/
 
     HAL_Delay(10);
 
     /*CANの初期化*/
 
-    main_bus.get_gain(hub_config);
+    while (1) {
+        if (main_silent_fdcan.buffer.nvic_.Rx_Callback) {
+            main_bus.get_gain(hub_config);
+            break;
+        }
+        HAL_Delay(1);
+    };
+
+    PID[0].set_gain(hub_config.kp, hub_config.ki, hub_config.kd); /*各モーター用のPID*/
+    PID[1].set_gain(hub_config.kp, hub_config.ki, hub_config.kd); /*各モーター用のPID*/
+    PID[2].set_gain(hub_config.kp, hub_config.ki, hub_config.kd); /*各モーター用のPID*/
+    PID[3].set_gain(hub_config.kp, hub_config.ki, hub_config.kd); /*各モーター用のPID*/
+
+    PID[0].set_control_cycle(1000); /*PIDの制御周期*/
+    PID[1].set_control_cycle(1000); /*PIDの制御周期*/
+    PID[2].set_control_cycle(1000); /*PIDの制御周期*/
+    PID[3].set_control_cycle(1000); /*PIDの制御周期*/
 
     HAL_TIM_Base_Start_IT(&htim6);
 
     HAL_Delay(50);
 
     /*キャリブレーション開始*/
-
-    four_omni_cab();
 
     /*モーター停止*/
 
@@ -136,155 +147,40 @@ void setup()
     HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, GPIO_PIN_SET);
 }
 
-void loop() {}
+void loop()
+{
+    if (esc_bus_can.buffer.nvic_.Rx_Callback) {
+        if ((esc_bus_can.buffer.nvic_.Id & 0x01) == 0x01) {
+            esc_bus_can.GetMessage(&c620_r[0]);
+            c620_send.value[0] = c620_rad_to_current(PID[0].PID(c620_receive[0].value.rpm) * 2 * M_PI);
+            esc_bus_can.SendMessage(&c620_s);
+        }
+
+        if ((esc_bus_can.buffer.nvic_.Id & 0x02) == 0x02) {
+            esc_bus_can.GetMessage(&c620_r[1]);
+            c620_send.value[1] = c620_rad_to_current(PID[1].PID(c620_receive[1].value.rpm) * 2 * M_PI);
+            esc_bus_can.SendMessage(&c620_s);
+        }
+
+        if ((esc_bus_can.buffer.nvic_.Id & 0x04) == 0x04) {
+            esc_bus_can.GetMessage(&c620_r[2]);
+            c620_send.value[2] = c620_rad_to_current(PID[2].PID(c620_receive[2].value.rpm) * 2 * M_PI);
+            esc_bus_can.SendMessage(&c620_s);
+        }
+
+        if ((esc_bus_can.buffer.nvic_.Id & 0x08) == 0x08) {
+            esc_bus_can.GetMessage(&c620_r[3]);
+            c620_send.value[3] = c620_rad_to_current(PID[3].PID(c620_receive[3].value.rpm) * 2 * M_PI);
+            esc_bus_can.SendMessage(&c620_s);
+        }
+    }
+}
 
 extern "C" void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef* htim)
 {
     if (htim == &htim6) {
         esc_bus_can.SendMessage_for_timer_loop(&c620_s);
     }
-}
-
-float P(float diff)
-{
-    return hub_config.kp * diff;
-}
-
-float I(float diff)
-{
-    static float last_diff = 0;
-    last_diff += diff;
-    return hub_config.ki * last_diff * 0.001;
-}
-
-float D(float diff)
-{
-    return hub_config.kd * diff;
-}
-
-float PID(float diff) {}
-
-void three_omni_cab() {}
-
-void four_omni_cab()
-{
-    /**
-     * 右上がID1, ID,4     ID,1
-     * 右下がID2,
-     * 左下がID3,
-     * 左上がID4, ID,3     ID,2
-     */
-
-    /**
-     * 前進　斜め　回転のデータを取得する。
-     * 0~300 rpmまでの値を取得
-     */
-
-    /*前進*/
-
-    for (uint16_t i = 0; i < 800; i++) {
-        c620_send.value[0] = i * 10;
-        c620_send.value[1] = i * 10;
-        c620_send.value[2] = -i * 10;
-        c620_send.value[3] = -i * 10;
-
-        esc_bus_can.SendMessage(&c620_s);
-        esc_bus_can.wait_tx_event_fin();
-        /*送信完了まで待つ*/
-        HAL_Delay(10);
-        /*モーターの速度が更新されるであろう時間待つ*/
-        /*10mSはあてずっぽう　他も同じ*/
-        /*すでにHAL_Delay()で待っているためwhileで待つ必要なし*/
-        if ((esc_bus_can.setup_type.buffer->nvic_.Id & 0x01) == 0x01) {
-            /*ID1*/
-            esc_bus_can.GetMessage(&c620_r[0]);
-            c620_line_table[0][i].c620_value = c620_send.value[0];
-            c620_line_table[0][i].rad_p_s    = (float)(c620_receive[0].value.rpm) / 60.0f * 2 * M_PI;
-            /*radian per second = rpm / minutes * 2 * π*/
-        } else {
-            Error();
-        }
-
-        if ((esc_bus_can.setup_type.buffer->nvic_.Id & 0x02) == 0x02) {
-            /*ID2*/
-            esc_bus_can.GetMessage(&c620_r[1]);
-            c620_line_table[1][i].c620_value = c620_send.value[1];
-            c620_line_table[1][i].rad_p_s    = (float)(c620_receive[1].value.rpm) / 60.0f * 2 * M_PI;
-        } else {
-            Error();
-        }
-
-        if ((esc_bus_can.setup_type.buffer->nvic_.Id & 0x04) == 0x04) {
-            /*ID3*/
-            esc_bus_can.GetMessage(&c620_r[2]);
-            c620_line_table[2][i].c620_value = c620_send.value[2];
-            c620_line_table[2][i].rad_p_s    = (float)(c620_receive[2].value.rpm) / 60.0f * 2 * M_PI;
-        } else {
-            Error();
-        }
-
-        if ((esc_bus_can.setup_type.buffer->nvic_.Id & 0x08) == 0x08) {
-            /*ID4*/
-            esc_bus_can.GetMessage(&c620_r[3]);
-            c620_line_table[3][i].c620_value = c620_send.value[3];
-            c620_line_table[3][i].rad_p_s    = (float)(c620_receive[3].value.rpm) / 60.0f * 2 * M_PI;
-        } else {
-            Error();
-        }
-    }
-
-    /*斜め*/
-    for (uint16_t i = 0; i < 800; i++) {
-        c620_send.value[0] = i * 10;
-        // c620_send.value[1] = i * 10;
-        c620_send.value[2] = -i * 10;
-        // c620_send.value[3] = -i * 10;
-
-        esc_bus_can.SendMessage(&c620_s);
-        esc_bus_can.wait_tx_event_fin();
-        /*送信完了まで待つ*/
-        HAL_Delay(10);
-        /*モーターの速度が更新されるであろう時間待つ*/
-
-        if ((esc_bus_can.setup_type.buffer->nvic_.Id & 0x01) == 0x01) {
-            /*ID1*/
-            esc_bus_can.GetMessage(&c620_r[0]);
-            c620_line_table[0][i].c620_value = c620_send.value[0];
-            c620_line_table[0][i].rad_p_s    = (float)(c620_receive[0].value.rpm) / 60.0f * 2 * M_PI;
-            /*radian per second = rpm / minutes * 2 * π*/
-        } else {
-            Error();
-        }
-
-        // if ((esc_bus_can.setup_type.buffer->nvic_.Id & 0x02) == 0x02) {
-        //     /*ID2*/
-        //     esc_bus_can.GetMessage(&c620_r[1]);
-        //     c620_line_table[1][i].c620_value = c620_send.value[1];
-        //     c620_line_table[1][i].rad_p_s    = (float)(c620_receive[1].value.rpm) / 60.0f * 2 * M_PI;
-        // } else {
-        //     Error();
-        // }
-
-        if ((esc_bus_can.setup_type.buffer->nvic_.Id & 0x04) == 0x04) {
-            /*ID3*/
-            esc_bus_can.GetMessage(&c620_r[2]);
-            c620_line_table[2][i].c620_value = c620_send.value[2];
-            c620_line_table[2][i].rad_p_s    = (float)(c620_receive[2].value.rpm) / 60.0f * 2 * M_PI;
-        } else {
-            Error();
-        }
-
-        // if ((esc_bus_can.setup_type.buffer->nvic_.Id & 0x08) == 0x08) {
-        //     /*ID4*/
-        //     esc_bus_can.GetMessage(&c620_r[3]);
-        //     c620_line_table[3][i].c620_value = c620_send.value[3];
-        //     c620_line_table[3][i].rad_p_s    = (float)(c620_receive[3].value.rpm) / 60.0f * 2 * M_PI;
-        // } else {
-        //     Error();
-        // }
-    }
-
-    /*回転*/
 }
 
 void Error() {}
