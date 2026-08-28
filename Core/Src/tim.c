ef50dfbeb16d63b6c0d08251648b758ea4ba3cfa
aiba-gento <8gn24gn25@gmail.com>
2026-08-28 17:43:28 +0900

    fix:エンコーダー4chのcouterperiodを修正

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -182,7 +182,7 @@ void MX_TIM4_Init(void)
   htim4.Instance = TIM4;
   htim4.Init.Prescaler = 0;
   htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim4.Init.Period = 100;
+  htim4.Init.Period = 65535;
   htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   sConfig.EncoderMode = TIM_ENCODERMODE_TI12;
